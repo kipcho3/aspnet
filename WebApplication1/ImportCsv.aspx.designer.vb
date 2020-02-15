db8eea9ca26da8b44d2852079b1f93e639aec085
kipcho0303 <kipcho0303@gmail.com>
2020-02-16 00:22:53 +0900

    importcsvをコミット

---
+++ WebApplication1/ImportCsv.aspx.designer.vb
@@ -0,0 +1,51 @@
+﻿'------------------------------------------------------------------------------
+' <自動生成>
+'     このコードはツールによって生成されました。
+'
+'     このファイルへの変更は、以下の状況下で不正な動作の原因になったり、
+'     コードが再生成されるときに損失したりします。 
+' </自動生成>
+'------------------------------------------------------------------------------
+
+Option Strict On
+Option Explicit On
+
+
+Partial Public Class ImportCsv
+
+    '''<summary>
+    '''form1 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
+
+    '''<summary>
+    '''FileUpload1 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents FileUpload1 As Global.System.Web.UI.WebControls.FileUpload
+
+    '''<summary>
+    '''btnUl コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents btnUl As Global.System.Web.UI.WebControls.Button
+
+    '''<summary>
+    '''Button1 コントロール。
+    '''</summary>
+    '''<remarks>
+    '''自動生成されたフィールド。
+    '''変更するには、フィールドの宣言をデザイナー ファイルから分離コード ファイルに移動します。
+    '''</remarks>
+    Protected WithEvents Button1 As Global.System.Web.UI.WebControls.Button
+End Class
---
+++ WebApplication1/ImportCsv.aspx.vb
@@ -0,0 +1,88 @@
+﻿Imports Microsoft.VisualBasic.FileIO
+Imports System.Data
+Imports System.Data.SqlClient
+
+Public Class ImportCsv
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+    End Sub
+
+    Protected Sub btnUl_Click(sender As Object, e As EventArgs) Handles btnUl.Click
+        Dim strFilePath As String = "C:\zwork\keibadata\file.csv"
+        Dim strEncode As String = "UTF-8"
+        Dim strSql As String = "insert into horse values(@a1, @a2, @a3,@a4, @a5, @a6)"
+        Dim bFirstFlg As Boolean = True
+
+        'DBアクセス
+        Dim con As ConnectionStringSettings = ConfigurationManager.ConnectionStrings("SelfAsp")
+
+        'ファイルを取り込む
+        Using csvfile As New TextFieldParser(strFilePath, System.Text.Encoding.GetEncoding(strEncode))
+            'CSVを指定
+            csvfile.TextFieldType = FieldType.Delimited
+
+            '区切り文字
+            csvfile.SetDelimiters(",")
+
+            'ファイル内容をループ
+            While Not csvfile.EndOfData
+                Dim row As String() = csvfile.ReadFields()
+
+                If bFirstFlg Then
+                    bFirstFlg = False
+                    Continue While
+                End If
+
+                '現在行を読み込み
+                Dim intColCnt As Integer = 0
+
+                'DBに突っ込む
+                Dim sql As New SqlCommand(strSql)
+
+                For Each col As String In row
+                    'debugとりあえず、コンソールに出してみる
+                    'Console.WriteLine(col)
+
+                    intColCnt += 1
+                    sql.Parameters.AddWithValue("@a" & intColCnt.ToString, col)
+                    If intColCnt = 6 Then Exit For
+                Next
+
+                Using db As New SqlConnection(con.ConnectionString)
+                    sql.Connection = db
+                    db.Open()
+                    Dim tran As SqlTransaction = db.BeginTransaction
+
+                    Try
+                        sql.Transaction = tran
+                        sql.ExecuteNonQuery()
+                        tran.Commit()
+                    Catch ex As Exception
+                        Response.Write(ex.ToString)
+                        tran.Rollback()
+                        Exit While
+                    Finally
+                        sql.Dispose()
+                        db.Close()
+                    End Try
+                End Using
+
+            End While
+        End Using
+    End Sub
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim setting As ConnectionStringSettings = ConfigurationManager.ConnectionStrings("SelfAsp")
+
+        Using db As New SqlConnection(setting.ConnectionString)
+            Dim comm As New SqlCommand("select title from Book")
+
+            comm.Connection = db
+            db.Open()
+            Dim reader As SqlDataReader = comm.ExecuteReader()
+            db.Close()
+        End Using
+    End Sub
+End Class
